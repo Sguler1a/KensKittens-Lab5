bd17940ea63558b0bd509bdf40716dfad2dd7a01
Eric Zhang <46907515+ericzhang11101@users.noreply.github.com>
2023-02-17 11:02:41 -0500

    Load restaurants upon first rendering RestaurantList

---
+++ opinion-ate/src/components/RestaurantList.spec.js
@@ -0,0 +1,22 @@
+describe('RestaurantList', () => {
+  it('loads restaurants on first render', () => {});
+});
+
+it('loads restaurants on first render', () => {
+  const loadRestaurants = jest.fn().mockName('loadRestaurants');
+});
+
+import {render} from '@testing-library/react';
+import RestaurantList from './RestaurantList';
+describe('RestaurantList', () => {
+  it('loads restaurants on first render', () => {
+    const loadRestaurants = jest.fn().mockName('loadRestaurants');
+    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+  });
+});
+
+it('loads restaurants on first render', () => {
+  const loadRestaurants = jest.fn().mockName('loadRestaurants');
+  render(<RestaurantList loadRestaurants={loadRestaurants} />);
+  expect(loadRestaurants).toHaveBeenCalled();
+});
